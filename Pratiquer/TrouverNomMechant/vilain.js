a20e3359a7b82edd9b0c467d25b93864ea6eee1c
Nils <72611176+Nils92@users.noreply.github.com>
2021-02-16 12:25:05 +0100

    Update vilain.js

--- Pratiquer/TrouverNomMechant/vilain.js
+++ Pratiquer/TrouverNomMechant/vilain.js
@@ -1,18 +1,21 @@
-let month = "April"
-//Mois->prénom
-let monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-let FirstNameArray = ["The Evil", "The Vile", "The Cruel", "The Trashy", "The Despicable", "The Embarrassing", "The Disreputable", "The Atrocious", "The Twirling", "The Orange", "The Terrifying", "The Awkward"]
-
-//dernier chiffre -> Nom de Famille
-let chiffreArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-let LastNameArray = ["Mustache","Pickle","Hood Ornament","Raisin","Recycling Bin","Potato","Tomato","House Cat","Teaspoon","Laundry Basket"]
-
-if (monthArray.includes(month)) {
+function getVillainName(birthday){
+  const m = ["Evil","Vile","Cruel","Trashy","Despicable","Embarrassing","Disreputable","Atrocious","Twirling","Orange","Terrifying","Awkward"];
+  const d = ["Mustache","Pickle","Hood Ornament","Raisin","Recycling Bin","Potato","Tomato","House Cat","Teaspoon","Laundry Basket"];
+  let monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  let chiffreArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  let indexnumber1
+  
+  const birthdayitems = birthday.split(' ');
+  let month = birthdayitems[0];
+  let day = birthdayitems[1];
+  
+   if (monthArray.includes(month) && day > 9) {
     const indexnumber = monthArray.indexOf(month);
-    console.log(FirstNameArray[indexnumber]);
-} else {
-    alert("Mois pas valide")
+    indexnumber1 = day % 10;
+    console.log("The " +  m[indexnumber] +" "+ d[indexnumber1]);
+  } else {
+    indexnumber1 = day
+    console.log("The " +  m[indexnumber] +" "+ d[indexnumber1]);
+  }
 }
-
-
-console.log(getVillainName("May 3"));
+getVillainName("December 17");
